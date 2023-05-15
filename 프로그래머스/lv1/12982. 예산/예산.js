9d5ed54b2d9555b4afc5ad50f5f4dfac3a248231
redcoin96 <129242822+redcoin96@users.noreply.github.com>
2023-05-15 10:22:52 +0900

    [level 1] Title: 예산, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -0,0 +1,12 @@
+function solution(d, budget) {
+    const arr = d.sort((a,b)=>a-b)
+    let sum = 0;
+    let count = 0;
+    for(let i = 0; i<arr.length; i++){
+        if (sum+arr[i] <= budget){
+            sum += arr[i]
+            count++
+        }
+    }
+    return count
+}
