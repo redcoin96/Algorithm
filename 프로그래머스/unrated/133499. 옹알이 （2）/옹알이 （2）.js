9bca5d6f7b9f9e75d02e93ffddc3aeb625c747c1
redcoin96 <129242822+redcoin96@users.noreply.github.com>
2023-06-01 00:50:57 +0900

    [unrated] Title: 옹알이 (2), Time: 0.44 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
@@ -1,15 +1,11 @@
-/*	
-["aya", "yee", "u"]
-*/
-
 function solution(babbling) {
    const arr = ['aya','ye','woo','ma']
    let count = 0;
    for(let i = 0; i<babbling.length; i++){
        let prev = ''
-       let stack = [] //['a','y','a']
+       let stack = [] 
        let isPossible = false
-       const curword = babbling[i].split('') //['a', 'y', 'a']
+       const curword = babbling[i].split('') 
        for(let j = 0; j<curword.length; j++){
            stack.push(curword[j])
            if(arr.includes(stack.join(''))){
@@ -25,7 +21,3 @@ function solution(babbling) {
   }
   return count;
 }
-    
-
-    
-    
