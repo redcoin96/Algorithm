580913f2e4eb1ae126b17a03b28f21efd0cb0ab5
redcoin96 <129242822+redcoin96@users.noreply.github.com>
2023-06-25 14:17:41 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.84 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,23 +1,11 @@
 function solution(A,B){
     let count = 0;
-    A = A.sort((a,b)=>b-a)
-    B = B.sort((a,b)=>b-a)
-    function func(arr1, arr2){
-        if(!arr1.length) return
-        if(arr1[0]>arr2[0]){
-            count += arr1[0]*arr2[arr2.length-1]
-            arr1.shift()
-            arr2.pop()
-            func(arr1,arr2)
-        } else {
-            count += arr2[0]*arr1[arr1.length-1]
-            arr1.pop()
-            arr2.shift()
-            func(arr1,arr2)
-        }
-    }
+    A.sort((a,b)=>a-b)
+    B.sort((a,b)=>b-a)
     
-    func(A,B)
+    for(let i = 0; i<A.length; i++){
+        count += A[i]*B[i]
+    }
     
     return count;
 }
