75f24181b04477ac468e7a86d80c20c5477a1f9f
redcoin96 <129242822+redcoin96@users.noreply.github.com>
2023-06-26 14:30:56 +0900

    Update 멀리 뛰기.js

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -12,4 +12,4 @@ function solution(n) {
 
 //피보나치 문제
 //f(i)=f(i-1)+f(i-2)
-//https://velog.io/@longroadhome/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-LV.3-%EB%A9%80%EB%A6%AC-%EB%9B%B0%EA%B8%B0-JS 
+//참고: https://velog.io/@longroadhome/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-LV.3-%EB%A9%80%EB%A6%AC-%EB%9B%B0%EA%B8%B0-JS 
